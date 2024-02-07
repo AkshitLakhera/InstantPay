1d7896aeff31646b556f69fdfdb2821c636775fa
“[Akshit <akshitlakhera14@gmail.com>
2024-02-07 13:01:38 +0530

    DB modified

--- backend/db/db.js
+++ backend/db/db.js
@@ -6,9 +6,32 @@ mongoose.connect('mongodb+srv://admin:18UnbuxKp5q8OEev@cluster0.f3e1pxb.mongodb.
 })
 // Creating mongoose schema
 const userSchema = new mongoose.Schema({
-    firstName:String,
-    lastName:String,
-    password:String,
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    firstName :{
+        type:String,
+        required:true,
+        trim:true,
+        maxLength:50
+    },
+    lastName: {
+        type:String,
+        required:true,
+        trim:true,
+        maxLength:50
+    },
+    password:{
+        type:String,
+        required:true,
+        minLength:6
+    }
 })
 
 //creating mongoose modal
