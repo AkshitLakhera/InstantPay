6386927cf65eb7d1e544f06b3b7eb4500ddd9027
“[Akshit <akshitlakhera14@gmail.com>
2024-02-13 18:57:35 +0530

    Added transaction logic in backend

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const authMiddlware =require("../middleware");
-const Account  = require("../db/db")
+const Account  = require("../db/db");
+const { default: mongoose } = require("mongoose");
 const router = express.Router();
+// Here in this app we  haven't made the route for adding balance to the account so we are adding random balance from our self when user get sign up
 // An endpoint for user to get their balance
 router.get("/balance",authMiddlware,async (req,res) => {
     const userId = req.userId;
@@ -11,5 +13,44 @@ router.get("/balance",authMiddlware,async (req,res) => {
     res.status(200).json({
         balance:account.balance
     })
+})
+//  An endpoint for user to transfer money to another account
+router.post("/transfer",authMiddlware,async (req,res) => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    const{to,amount} = req.body;
+    const account =  await Account.findOne({userId:req.userId}).session(session);
+    if (!account || account.balance <amount){
+        await session.abortTransaction();
+        res.status(400).json({
+            message: "Insufficient balance" 
+        })
+    }
+    // checking transfer account is really exisits 
+    const toAccount =  await Account.findOne({userId:to}).session(session);
+    if(!toAccount){
+        await session.abortTransaction();
+        res.status(400).json({
+            message: "Invalid account" 
+        })
+    }
+    // If all conditions passes we will send money
+    // first account which is going to transfer will get deducted first
+    await Account.updateOne({userId:req.userId},{
+        $inc:{
+            balance: -amount
+        }
+    })
+    // Account which is coming to this account is added
+    await Account.updateOne({userId:to},{
+        $inc:{
+            balance: +amount
+        }
+    })
+    // Commit the transaction
+    await session.commitTransaction();
+    res.json({
+        message: "Transfer successful"
+    });
 })
 module.exports=router;
