0c15c225a248f52128e0f850c3c70b6b423ae978
“[Akshit <akshitlakhera14@gmail.com>
2024-02-13 18:01:59 +0530

    Added get route to get  account balance

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,3 +1,15 @@
 const express = require("express");
+const authMiddlware =require("../middleware");
+const Account  = require("../db/db")
 const router = express.Router();
+// An endpoint for user to get their balance
+router.get("/balance",authMiddlware,async (req,res) => {
+    const userId = req.userId;
+    const account = await Account.findOne({
+        userId
+    });
+    res.status(200).json({
+        balance:account.balance
+    })
+})
 module.exports=router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,6 @@ const userRouter = require('./user');
 const accountRouter=require('./account');
 const router = express.Router();
 // Import the userRouter in backend/routes/index.js so all requests to /api/v1/user get routed to the userRouter.
-
 router.use("/user",userRouter);
 router.use("/account",accountRouter);
 module.exports = router;
