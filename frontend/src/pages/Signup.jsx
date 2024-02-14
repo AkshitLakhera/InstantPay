0253e4833d41d3e3f938f6e1f8d4619a3c961f4f
“[Akshit <akshitlakhera14@gmail.com>
2024-02-14 21:55:45 +0530

    Debug signup

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -4,18 +4,20 @@ import { Heading } from "../components/Heading"
 import { InputBox } from "../components/InputBox"
 import { SubHeading } from "../components/SubHeading"
 
-export const Signin = () => {
+export const Signup = () => {
     return <div className="bg-slate-300 h-screen flex justify-center">
     <div className="flex flex-col justify-center">
       <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
-        <Heading label={"Sign in"} />
-        <SubHeading label={"Enter your credentials to access your account"} />
+        <Heading label={"Sign up"} />
+        <SubHeading label={"Enter your infromation to create an account"} />
+        <InputBox placeholder="John" label={"First Name"} />
+        <InputBox placeholder="Doe" label={"Last Name"} />
         <InputBox placeholder="harkirat@gmail.com" label={"Email"} />
         <InputBox placeholder="123456" label={"Password"} />
         <div className="pt-4">
-          <Button label={"Sign in"} />
+          <Button label={"Sign up"} />
         </div>
-        <BottomWarning label={"Don't have an account?"} buttonText={"Sign up"} to={"/signup"} />
+        <BottomWarning label={"Already have an account?"} buttonText={"Sign in"} to={"/signin"} />
       </div>
     </div>
   </div>
