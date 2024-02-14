49e5435decee4a5ac06b238b7747f918ae4660d8
“[Akshit <akshitlakhera14@gmail.com>
2024-02-14 21:36:02 +0530

    Sign in and dashboard is done

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -5,7 +5,6 @@ import { InputBox } from "../components/InputBox"
 import { SubHeading } from "../components/SubHeading"
 
 export const Signin = () => {
-
     return <div className="bg-slate-300 h-screen flex justify-center">
     <div className="flex flex-col justify-center">
       <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
