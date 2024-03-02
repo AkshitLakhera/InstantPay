e336e399d53e75e8961ad2f325dd6fb78dfd695b
“[Akshit <akshitlakhera14@gmail.com>
2024-03-02 12:05:19 +0530

    button arrow added with transition

--- frontend/src/components/Landinpage.jsx
+++ frontend/src/components/Landinpage.jsx
@@ -41,12 +41,15 @@ export default function Landing() {
           <div className="m_header">
             <h1>Build Payment Gateways Trust With UpTrack Intellgence</h1>
           </div>
-          <div className="m_para"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore?</p></div>
-          <button>Get Started </button>
+          <div className="m_para"><p>Establish Trust with payment Gateway By Synchronizing Shipment information.</p></div>
+          <button className="arrow-button bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">Get Started <span className="arrow"></span>
+</button>
+
 
         </div>
         <div className="m_img">
           <img src={image} alt="" />
+          
         </div>
       </div>
 
